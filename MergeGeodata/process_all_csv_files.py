bf2774d26b5db77d674dd7059b7f7d43e4f30692
Richard Ellicott <mescalin@gmail.com>
2021-12-16 03:00:32 +0000

    .

---
+++ MergeGeodata/process_all_csv_files.py
@@ -0,0 +1,205 @@
+"""
+
+
+
+Cities Population:
+https://public.opendatasoft.com/explore/dataset/geonames-all-cities-with-a-population-1000/export/?disjunctive.cou_name_en&sort=name
+
+filename: geonames-all-cities-with-a-population-1000.csv
+filesize: 25'937 KB
+liscense: Attribution 4.0 International (CC BY 4.0)
+
+
+
+
+GDP (countries):
+https://data.worldbank.org/indicator/NY.GDP.MKTP.CD
+
+filename: API_NY.GDP.MKTP.CD_DS2_en_csv_v2_3358362.csv
+size: 256 KB
+
+
+
+take the world data, format it to just 2020 GDP:
+
+
+
+
+"""
+import csv
+
+
+
+
+
+
+class Main():
+
+    thisdict = {
+        "brand": "Ford",
+        "model": "Mustang",
+        "year": 1964
+    }
+
+
+
+    def read_csv_file(self,filename, predicate, header_row = 0, delimiter=',', quotechar='"',encoding='utf-8',max_records = 1000000):
+
+
+        csv_reader = csv.reader(
+            open(filename, 'r', encoding=encoding), delimiter=delimiter, quotechar=quotechar)
+
+
+        i_max = max_records
+
+        i = 0
+
+        record_id = 0
+
+        keys = {}
+
+        for line in csv_reader:
+
+
+            if i < header_row:
+                pass
+            elif i == header_row:
+
+                i2 = 0
+                for label in line:
+                    keys[label] = i2
+
+
+                    i2 += 1
+                print("keys: ", keys)
+            else:
+                # print("ff")
+                predicate(record_id,line, keys)
+                record_id += 1
+
+            
+
+            i += 1
+            if i > i_max:
+                break
+
+
+
+    country_code_to_gdp = {}
+    country_name_to_gdp = {}
+
+
+    def counties_gdp_data_predicate(self,i, row, keys):
+
+        # print("gg")
+
+        if len(row) > 1:
+
+
+            country_name = row[keys['Country Name']]
+            country_code = row[keys['Country Code']]
+            gdp_2020 = row[keys['2020']]
+
+            try:
+                gdp_2020 = float(gdp_2020)
+            except ValueError as e:
+                # print("Variable x is not defined ",e)
+                gdp_2020 = 0.0
+
+
+
+
+
+            self.country_code_to_gdp[country_code] = gdp_2020
+
+            self.country_name_to_gdp[country_name] = gdp_2020
+
+            # print("{} {}  {}".format(country_name,country_code,gdp_2020))
+            pass
+
+    
+
+
+
+
+    cities_count = 0
+
+
+    def count_add(self):
+        self.record_count22 += 1
+        pass
+
+    def cities_pop_data_csv_predicate(self,i, row, keys):
+
+        # print("gg")
+
+        if len(row) > 1:
+
+
+            name = row[keys['Name']]
+
+            country_name = row[keys['Country name EN']]
+            
+
+            country_code = row[keys['Country Code']]
+
+            population = row[keys['Population']]
+            population = int(population)
+
+            gdp_link = None
+
+
+            try:
+                # gdp_link = self.country_code_to_gdp[country_code]
+                gdp_link = self.country_name_to_gdp[country_name]
+
+            except:
+
+                # print("nod gdp found: ", country_name)
+
+                pass
+
+
+
+
+            if population > 2000000:
+                # print("{}  {} {}  {}".format(name,country_name,population,gdp_link))
+
+                self.cities_count += 1
+
+                if gdp_link == None:
+                    print("No GDP found for city \"{}\" looking for country  \"{}\"" .format(  name, country_name))
+                
+
+
+            # country_code = row[keys['Country Code']]
+            # gdp_2020 = row[keys['2020']]
+
+            # print("{}   {}  {}  {}".format(i,name,country_name,population))
+            pass
+
+
+    counties_gdp_data =  "API_NY.GDP.MKTP.CD_DS2_en_csv_v2_3358362.csv" # comma
+
+    cities_pop_data_csv =  "geonames-all-cities-with-a-population-1000.csv" # semicolon
+
+
+    def __init__(self):
+
+        
+
+        self.read_csv_file(self.counties_gdp_data,self.counties_gdp_data_predicate, 4, delimiter=',',max_records = 1000000)
+
+
+
+
+    
+        
+
+        self.read_csv_file(self.cities_pop_data_csv,self.cities_pop_data_csv_predicate, 0, delimiter=';',max_records = 1000000)
+
+        print("cities_count: ",self.cities_count)
+
+
+
+Main()
---
+++ MergeGeodata/readme.md
@@ -0,0 +1,39 @@
+
+
+
+
+
+a project to compile these data sources
+
+
+
+city populations
+
+with country gdps
+
+
+
+
+note have modified file:
+API_NY.GDP.MKTP.CD_DS2_en_csv_v2_3358362.ucsv
+
+
+
+
+
+city pop:
+https://public.opendatasoft.com/explore/dataset/geonames-all-cities-with-a-population-1000/export/?disjunctive.cou_name_en&sort=name
+geonames-all-cities-with-a-population-1000.csv
+25'295 KB
+
+
+country gdp:
+https://data.worldbank.org/indicator/NY.GDP.MKTP.CD
+API_NY.GDP.MKTP.CD_DS2_en_csv_v2_3358362.csv
+256 KB
+
+
+country pop:
+https://data.worldbank.org/indicator/SP.POP.TOTL
+
+
